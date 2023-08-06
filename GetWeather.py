3fd212e91720ed6ea6aee8f3ca43a0c2f6776933
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2023-08-06 17:25:39 +0900

    Update GetWeather.py

--- GetWeather.py
+++ GetWeather.py
@@ -1,7 +1,17 @@
 import requests
 import json
+import streamlit as st
 
-city = "Seoul" #도시
+
+option = st.selectbox(
+    '나라의 날씨',
+    ('Korea', 'Philippines'))
+
+if option == "Korea":
+    city = "Suwon" #도시
+elif option == "Philippines":
+    city = "Tacloban"
+
 apiKey = "714b1ecad9eaa65fbd2818d147b3bc89"
 lang = 'kr' #언어
 units = 'metric' #화씨 온도를 섭씨 온도로 변경
@@ -11,9 +21,12 @@ result = requests.get(api)
 result = json.loads(result.text)
 
 name = result['name']
-weather = result['description'][0]['main']
+weather = result['weather'][0]['description']
 temperature = result['main']['temp']
+wind = result['wind']['speed']
+humidity = result['main']['humidity']
 
-print(name)
-print(weather)
-print(temperature)
+col1, col2, col3 = st.columns(3)
+col1.metric("온도", temperature, "1.2 °F")
+col2.metric("바람", wind, "-8%")
+col3.metric("습기", humidity, "4%")
