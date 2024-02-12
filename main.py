ac9fe4a41fef918651ded705939458a5651b6cd1
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-02-12 17:27:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ try:
 
     # Extract city information
     city = locname.raw.get('address', {}).get('city', 'City not found')
-    city
 except TypeError:
     #st.success('Please wait. You location information is getting.')
     city = '수원시'
@@ -43,12 +42,11 @@ apiKey = "714b1ecad9eaa65fbd2818d147b3bc89"
 lang = 'kr' #언어
 units = 'metric' #화씨 온도를 섭씨 온도로 변경
 api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&lang={lang}&units={units}"
-#weather = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+weather = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
 
 result = requests.get(api)
 result = json.loads(result.text)
 
-st.markdown(result)
 name = result['name']
 weather = result['weather'][0]['description']
 temperature = result['main']['temp']
